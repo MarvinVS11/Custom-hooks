37b5fcaafce506bd2d899bb5b794964882855af0
Marvin <marvi11@hotmail.es>
2022-09-27 14:27:46 -0600

    primer commit

---
+++ useCounter.js
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+export const useCounter= (initialValue = 10)=>{
+
+    const [counter, setCounter] = useState(initialValue)
+
+     const increment =(value)=>{
+        setCounter((counter)=>counter + 1);
+    }
+     const decrement=(value)=>{
+       // if(counter ==0) return;
+        setCounter(counter - 1);
+    }
+     const reset = ()=>{
+        setCounter(initialValue);
+    }
+
+    return{
+        counter,
+        increment,
+        decrement,
+        reset
+    }
+}
